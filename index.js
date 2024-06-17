70522377c46bb9c88f585b2b523a26c437716dfb
ben mccarthy <bmccarthy90@gmail.com>
2024-06-17 15:46:48 -0400

    DOM mod lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// test 1
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+// test 2 newHeader variable that points to <h1> node
+const newHeader = document.createElement('h1');
+
+// test 3 add 'victory' id to <h1> node
+newHeader.setAttribute("id", 'victory');
+
+// test 4 add name as text to <h1> node
+newHeader.textContent = "Ben McCarthy is the champion";
